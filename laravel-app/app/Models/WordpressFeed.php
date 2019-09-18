a2920740bf52c8e1f20303252cd51184eb33c576
Div <b510336@gmail.com>
2019-09-19 01:36:44 +0800

    create WordpressFeed Model

---
+++ laravel-app/app/Models/WordpressFeed.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WordpressFeed extends Model
+{
+    protected $fillable = [
+        'source',
+        'post_id',
+        'created_at',
+        'updated_at',
+    ];
+}
